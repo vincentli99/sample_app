8046f782c043a92e687910d74ce5b58f5f309c00
Vincent Li <road1109@gmail.com>
2014-10-25 21:14:29 +0800

    finished chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def autthenticated?(remember_token)
+  def authenticated?(remember_token)
   	return false if remember_digest.nil?
   	BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
